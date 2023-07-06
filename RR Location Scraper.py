29fb12f8f3ac9364d451c5d1b7fda61b6dc55297
zdcoffin <123233759+zdcoffin@users.noreply.github.com>
2023-07-05 21:46:19 -0500

    Add files via upload
    Completed the next function to scrape from the fishing and surfing locations sheet. The functions for the two largest sheets are now complete.

--- RR Location Scraper.py
+++ RR Location Scraper.py
@@ -5,9 +5,10 @@ gc = gspread.service_account(filename= 'creds.json')
 
 RR_sheet = gc.open("RR Locations Test")
 
-grass_caves_worksheet = RR_sheet.get_worksheet(0)
 
 def get_grass_and_caves_data(pokemon):
+    
+    grass_caves_worksheet = RR_sheet.get_worksheet(0)
     
     pokemon_location_cells = grass_caves_worksheet.findall(pokemon)
 
@@ -41,9 +42,50 @@ def get_grass_and_caves_data(pokemon):
     return area_dict
     
 
-print(get_grass_and_caves_data("Dragapult"))
+
+def get_fishing_and_surfing_data(pokemon):
+
+    fishing_surfing_worksheet = RR_sheet.get_worksheet(1)
+
+    pokemon_location_cells = fishing_surfing_worksheet.findall(pokemon)
+
+    area_dict = {}
+
+    for cell in pokemon_location_cells:
+        
+        row = cell.row
+        column = cell.col
+        name = cell.value
+
+        location = fishing_surfing_worksheet.cell(3, (column - 1)).value
+
+        which_rod_or_surf = ""
+
+        if row < 7:
+            which_rod_or_surf = "Old Rod"
+        elif row > 9 and row < 13:
+            which_rod_or_surf = "Good Rod"
+        elif row > 15 and row < 21:
+            which_rod_or_surf = "Super Rod"
+        elif row > 23:
+            which_rod_or_surf = "Surfing"
+        
+
+        percent = int((fishing_surfing_worksheet.cell(row, (column - 2)).value)[:-1])
+
+        if location not in area_dict:
+            area_dict[location] = [which_rod_or_surf, percent]
+        elif location in area_dict and which_rod_or_surf in area_dict[location]:
+            area_dict[location][(area_dict[location].index(which_rod_or_surf)) + 1] += percent
+        elif location in area_dict and which_rod_or_surf not in area_dict[location]:
+            area_dict[location].append(which_rod_or_surf)
+            area_dict[location].append(percent)
+    
+    return area_dict
 
 
+
+print(get_fishing_and_surfing_data("Sealeo"))
 
 
 
